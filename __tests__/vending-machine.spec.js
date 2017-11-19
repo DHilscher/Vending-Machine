4d1438430a4d1afb0753d9d3668d17d7d2fca3e6
David Hilscher <davidhilscher@gmail.com>
2017-11-18 19:11:02 -0800

    Initial commit.

---
+++ __tests__/vending-machine.spec.js
@@ -0,0 +1,9 @@
+const VendingMachine = require("../lib/vending-machine");
+
+describe("VendingMachine", () => {
+  describe("Print inventory.", () => {});
+  describe("Refill inventory.", () => {});
+  describe("Re-supply inventory.", () => {});
+  describe("Dispense inventory based on payment.", () => {});
+  describe("Return change.", () => {});
+});
